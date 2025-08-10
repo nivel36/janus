e88bea5dd984e6a56287fd3bb8619b456f2fd278
Abel Ferrer <aferre@nivel36.es>
2025-08-10 21:54:37 +0200

    Added health endpoint

--- src/main/java/es/nivel36/janus/JanusApplication.java
+++ src/main/java/es/nivel36/janus/JanusApplication.java
@@ -17,8 +17,12 @@ package es.nivel36.janus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan("es.nivel36.janus.service")
+@EnableJpaRepositories(basePackages = "es.nivel36.janus.service")
 public class JanusApplication {
 
 	public static void main(String[] args) {
