395b9b091f69efd6b4a6a6a55a876d93fd9dc320
Abel Ferrer <aferrer@nivel36.es>
2025-10-14 21:32:57 +0200

    Moved ZoneIdConverter from service.worksite to service package

--- src/main/java/es/nivel36/janus/service/ZoneIdConverter.java
+++ src/main/java/es/nivel36/janus/service/ZoneIdConverter.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package es.nivel36.janus.service.worksite;
+package es.nivel36.janus.service;
 
 import java.time.ZoneId;
 
