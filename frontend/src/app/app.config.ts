8f83529c841bb7fa1bd2d6277b662478c9057162
Abel Ferrer <aferrer@nivel36.es>
2025-10-26 13:23:17 +0100

    Revert "feat(frontend): migrate dashboard to primeng (#14)"
    This reverts commit 874d164c132c4c20862dd6b02dae539b2bf733cb.

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,7 +1,5 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
-import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideHttpClient(withInterceptorsFromDi()), provideAnimations()]
+  providers: []
 };
