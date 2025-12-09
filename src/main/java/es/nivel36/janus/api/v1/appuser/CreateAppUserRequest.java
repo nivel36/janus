54a56827eedf3a8e082f4472d3d790ce140986a7
Abel Ferrer <aferrer@nivel36.es>
2025-12-09 21:59:37 +0100

    Locale regexp has been changed to prevent stack overflows.

--- src/main/java/es/nivel36/janus/api/v1/appuser/CreateAppUserRequest.java
+++ src/main/java/es/nivel36/janus/api/v1/appuser/CreateAppUserRequest.java
@@ -53,8 +53,8 @@ public record CreateAppUserRequest( //
 		String surname, //
 
 		@NotBlank(message = "locale must not be blank") //
-		@Pattern(regexp = "^[A-Za-z]{2,3}(?:-[A-Za-z]{4})?(?:-(?:[A-Za-z]{2}|\\d{3}))?(?:-[A-Za-z0-9]{1,8})*$",
-			    message = "locale must be a valid BCP 47 language tag") //
+		@Pattern(regexp = "^[A-Za-z]{2,3}(?:-[A-Za-z]{4})?(?:-(?:[A-Za-z]{2}|\\d{3}))?(?:-[A-Za-z0-9][A-Za-z0-9]{0,7})*$", //
+		message = "locale must be a valid BCP 47 language tag") //
 		String locale, //
 
 		@NotNull(message = "timeFormat must not be null") //
