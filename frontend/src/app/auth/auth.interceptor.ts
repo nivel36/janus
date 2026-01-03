34704aa755abaad74d0d90c67812db3e30cc3e07
nivel36 <84566387+nivel36@users.noreply.github.com>
2026-01-03 21:18:48 +0100

    Add Angular login screen and seed password (#26)

---
+++ frontend/src/app/auth/auth.interceptor.ts
@@ -0,0 +1,17 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { AuthService } from './auth.service';
+
+export const authInterceptor: HttpInterceptorFn = (request, next) => {
+	const token = inject(AuthService).getToken();
+	if (!token) {
+		return next(request);
+	}
+
+	const withAuth = request.clone({
+		setHeaders: {
+			Authorization: `Bearer ${token}`
+		}
+	});
+	return next(withAuth);
+};
