4f5b2f65d1bd81c6f867055fa8e3bba99b2ac5a7
Abel Ferrer <aferrer@nivel36.es>
2026-01-03 18:34:20 +0100

    Copyright notice

--- src/main/java/es/nivel36/janus/service/workshift/TimeLogsExtractor.java
+++ src/main/java/es/nivel36/janus/service/workshift/TimeLogsExtractor.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2025 Abel Ferrer Jiménez
+ * Copyright 2026 Abel Ferrer Jiménez
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
