86cbf1dd72055985f33bbd4d844cdb4fa5876649
nivel36 <84566387+nivel36@users.noreply.github.com>
2026-04-01 00:10:04 +0200

    feat(frontend): add footer main menu with large icon actions (#101)

--- apps/frontend/src/app/shared/layout/page-template/page-template.component.ts
+++ apps/frontend/src/app/shared/layout/page-template/page-template.component.ts
@@ -1,10 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { TranslatePipe } from '@ngx-translate/core';
 
+import { MainMenuComponent } from '../../../core/layout/main-menu/main-menu.component';
+
 @Component({
   selector: 'app-page-template',
   standalone: true,
-  imports: [TranslatePipe],
+  imports: [TranslatePipe, MainMenuComponent],
   templateUrl: './page-template.component.html',
   styleUrl: './page-template.component.css',
 })
