2ca5cef6c1e002be2fe1549327d7e10f05d028fd
Abel Ferrer Jiménez <aferrer@nivel36.es>
2026-04-02 17:23:54 +0200

    Fixes app.component test

--- apps/frontend/src/app/app.component.spec.ts
+++ apps/frontend/src/app/app.component.spec.ts
@@ -2,12 +2,15 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 import { TestBed } from '@angular/core/testing';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
+      providers: [provideHttpClient(), provideHttpClientTesting()],
     }).compileComponents();
   });
 
