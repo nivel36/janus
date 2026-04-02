4b36946503ad7dcf782d640574dc2093d11ad5a2
nivel36 <84566387+nivel36@users.noreply.github.com>
2026-04-02 21:39:58 +0200

    Make Janus header brand navigate to root (#111)

--- apps/frontend/src/app/core/layout/page-template/page-template.component.ts
+++ apps/frontend/src/app/core/layout/page-template/page-template.component.ts
@@ -1,12 +1,13 @@
 import { Component, Input } from '@angular/core';
 import { TranslatePipe } from '@ngx-translate/core';
+import { RouterLink } from '@angular/router';
 
 import { MainMenuComponent } from '../../../core/layout/main-menu/main-menu.component';
 
 @Component({
   selector: 'app-page-template',
   standalone: true,
-  imports: [TranslatePipe, MainMenuComponent],
+  imports: [TranslatePipe, RouterLink, MainMenuComponent],
   templateUrl: './page-template.component.html',
   styleUrl: './page-template.component.css',
 })
