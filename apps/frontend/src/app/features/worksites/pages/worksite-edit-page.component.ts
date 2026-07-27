c10cb40df0be73ed9d608943b29198947467d153
Abel Ferrer <aferrer@nivel36.es>
2026-07-27 10:01:16 +0200

    Fixed input style

--- apps/frontend/src/app/features/worksites/pages/worksite-edit-page.component.ts
+++ apps/frontend/src/app/features/worksites/pages/worksite-edit-page.component.ts
@@ -12,6 +12,7 @@ import { TimezoneOption } from '../../../shared/models/timezone-option.model';
 import { AutocompleteTextboxComponent } from '../../../shared/ui/autocomplete-textbox/autocomplete-textbox.component';
 import { ButtonComponent } from '../../../shared/ui/button/button.component';
 import { FieldComponent } from '../../../shared/ui/field/field.component';
+import { InputComponent } from '../../../shared/ui/input/input.component';
 import { SelectComponent, SelectOption } from '../../../shared/ui/select/select.component';
 import { retryTransientHttpErrors } from '../../../shared/utils/http-retry.util';
 import {
@@ -30,6 +31,7 @@ import { WorksiteApiService } from '../services/worksite-api.service';
     AutocompleteTextboxComponent,
     ButtonComponent,
     FieldComponent,
+    InputComponent,
     SelectComponent,
     PageTemplateComponent,
   ],
